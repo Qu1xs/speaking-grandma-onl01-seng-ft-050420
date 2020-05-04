740cd4b16c409091ca3e138abf11edaab52c14d6
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 13:13:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == 
+  if string == "I LOVE YOU GRANDMA!"
+    puts 
     
   elsif string == string.uppercase()
     puts "HUH?! SPEAK UP, SONNY!"
